26e7c759fc69d4ca7b58224c9547c2f84d1dc36a
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-11-04 20:16:30 +0000

    Implement forward command in all directions

--- rover.go
+++ rover.go
@@ -11,6 +11,17 @@ const (
 	West
 )
 
+// Action constants
+const (
+	forward  = 'F'
+	backward = 'B'
+	left     = 'L'
+	right    = 'R'
+)
+
+// GridSize represents the size of the grid on Pluto (NxN)
+const GridSize = 100
+
 // Rover represents the state of a Kata rover
 type Rover struct {
 	x int
@@ -19,6 +30,24 @@ type Rover struct {
 }
 
 // Move takes in a set of commands for the rover to perform
-func (r Rover) Move(c string) {
+func (r *Rover) Move(cs string) {
+	for _, c := range cs {
+		switch c {
+		case forward:
+			r.forward()
+		}
+	}
+}
 
+func (r *Rover) forward() {
+	switch r.d {
+	case North:
+		r.y = (r.y + 1) % GridSize
+	case East:
+		r.x = (r.x + 1) % GridSize
+	case South:
+		r.y = (r.y + GridSize - 1) % GridSize
+	case West:
+		r.x = (r.x + GridSize - 1) % GridSize
+	}
 }
