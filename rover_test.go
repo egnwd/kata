4185e4223e13baf442b94c2db085f42098214fde
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-11-04 20:17:41 +0000

    Test moving backwards

--- rover_test.go
+++ rover_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Move Forward
+
 func TestForwardNorth(t *testing.T) {
 	r := Rover{x: 10, y: 10, d: North}
 
@@ -32,4 +34,34 @@ func TestForwardWest(t *testing.T) {
 
 	r.Move("F")
 	assert.Equal(t, Rover{x: 9, y: 10, d: West}, r)
+}
+
+// Move Backward
+
+func TestBackwardNorth(t *testing.T) {
+	r := Rover{x: 10, y: 10, d: North}
+
+	r.Move("B")
+	assert.Equal(t, Rover{x: 10, y: 9, d: North}, r)
+}
+
+func TestBackwardEast(t *testing.T) {
+	r := Rover{x: 10, y: 10, d: East}
+
+	r.Move("B")
+	assert.Equal(t, Rover{x: 9, y: 10, d: East}, r)
+}
+
+func TestBackwardSouth(t *testing.T) {
+	r := Rover{x: 10, y: 10, d: South}
+
+	r.Move("B")
+	assert.Equal(t, Rover{x: 10, y: 11, d: South}, r)
+}
+
+func TestBackwardWest(t *testing.T) {
+	r := Rover{x: 10, y: 10, d: West}
+
+	r.Move("B")
+	assert.Equal(t, Rover{x: 11, y: 10, d: West}, r)
 }
